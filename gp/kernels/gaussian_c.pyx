3fb116c72599ecb9072cfaeba3572004f3d3a257
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-11-25 20:43:36 -0800

    Use double, not float!

--- gp/kernels/gaussian_c.pyx
+++ gp/kernels/gaussian_c.pyx
@@ -8,22 +8,22 @@ from math import sqrt, exp, pi
 DTYPE = np.float64
 ctypedef np.float64_t DTYPE_t
 
-def K(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w):
+def K(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = 0.5*sqrt(2)*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*sqrt(w**2))
 
 
-def jacobian(np.ndarray[DTYPE_t, ndim=3] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w):
+def jacobian(np.ndarray[DTYPE_t, ndim=3] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
@@ -31,11 +31,11 @@ def jacobian(np.ndarray[DTYPE_t, ndim=3] out, np.ndarray[DTYPE_t, ndim=1] x1, np
             out[1, i, j] = 0.5*sqrt(2)*d**2*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**3*sqrt(w**2)) - 0.5*sqrt(2)*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w*sqrt(w**2))
 
 
-def hessian(np.ndarray[DTYPE_t, ndim=4] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w):
+def hessian(np.ndarray[DTYPE_t, ndim=4] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
@@ -47,66 +47,66 @@ def hessian(np.ndarray[DTYPE_t, ndim=4] out, np.ndarray[DTYPE_t, ndim=1] x1, np.
             out[1, 1, i, j] = 0.5*sqrt(2)*d**4*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**6*sqrt(w**2)) - 2.5*sqrt(2)*d**2*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**4*sqrt(w**2)) + sqrt(2)*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**2*sqrt(w**2))
 
 
-def dK_dh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w):
+def dK_dh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = sqrt(2)*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*sqrt(w**2))
 
 
-def dK_dw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w):
+def dK_dw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = 0.5*sqrt(2)*d**2*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**3*sqrt(w**2)) - 0.5*sqrt(2)*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w*sqrt(w**2))
 
 
-def d2K_dhdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w):
+def d2K_dhdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = sqrt(2)*exp(-0.5*d**2/w**2)/(sqrt(pi)*sqrt(w**2))
 
 
-def d2K_dhdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w):
+def d2K_dhdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = sqrt(2)*d**2*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**3*sqrt(w**2)) - sqrt(2)*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w*sqrt(w**2))
 
 
-def d2K_dwdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w):
+def d2K_dwdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = sqrt(2)*d**2*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**3*sqrt(w**2)) - sqrt(2)*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w*sqrt(w**2))
 
 
-def d2K_dwdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w):
+def d2K_dwdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
--- gp/kernels/periodic_c.pyx
+++ gp/kernels/periodic_c.pyx
@@ -8,22 +8,22 @@ from math import sqrt, exp, pi, sin, cos
 DTYPE = np.float64
 ctypedef np.float64_t DTYPE_t
 
-def K(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def K(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)
 
 
-def jacobian(np.ndarray[DTYPE_t, ndim=3] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def jacobian(np.ndarray[DTYPE_t, ndim=3] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
@@ -32,11 +32,11 @@ def jacobian(np.ndarray[DTYPE_t, ndim=3] out, np.ndarray[DTYPE_t, ndim=1] x1, np
             out[2, i, j] = 2.0*d*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)*cos(0.5*d/p)/(p**2*w**2)
 
 
-def hessian(np.ndarray[DTYPE_t, ndim=4] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def hessian(np.ndarray[DTYPE_t, ndim=4] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
@@ -54,132 +54,132 @@ def hessian(np.ndarray[DTYPE_t, ndim=4] out, np.ndarray[DTYPE_t, ndim=1] x1, np.
             out[2, 2, i, j]= d**2*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)**2/(p**4*w**2) - 1.0*d**2*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*cos(0.5*d/p)**2/(p**4*w**2) + 4.0*d**2*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)**2*cos(0.5*d/p)**2/(p**4*w**4) - 4.0*d*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)*cos(0.5*d/p)/(p**3*w**2)
 
 
-def dK_dh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def dK_dh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = 2.0*h*exp(-2.0*sin(0.5*d/p)**2/w**2)
 
 
-def dK_dw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def dK_dw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = 4.0*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)**2/w**3
 
 
-def dK_dp(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def dK_dp(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = 2.0*d*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)*cos(0.5*d/p)/(p**2*w**2)
 
 
-def d2K_dhdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def d2K_dhdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = 2.0*exp(-2.0*sin(0.5*d/p)**2/w**2)
 
 
-def d2K_dhdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def d2K_dhdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = 8.0*h*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)**2/w**3
 
 
-def d2K_dhdp(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def d2K_dhdp(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = 4.0*d*h*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)*cos(0.5*d/p)/(p**2*w**2)
 
 
-def d2K_dwdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def d2K_dwdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = 8.0*h*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)**2/w**3
 
 
-def d2K_dwdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def d2K_dwdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = -12.0*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)**2/w**4 + 16.0*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)**4/w**6
 
 
-def d2K_dwdp(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def d2K_dwdp(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = -4.0*d*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)*cos(0.5*d/p)/(p**2*w**3) + 8.0*d*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)**3*cos(0.5*d/p)/(p**2*w**5)
 
 
-def d2K_dpdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def d2K_dpdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = 4.0*d*h*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)*cos(0.5*d/p)/(p**2*w**2)
 
 
-def d2K_dpdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def d2K_dpdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
             out[i, j] = -4.0*d*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)*cos(0.5*d/p)/(p**2*w**3) + 8.0*d*h**2*exp(-2.0*sin(0.5*d/p)**2/w**2)*sin(0.5*d/p)**3*cos(0.5*d/p)/(p**2*w**5)
 
 
-def d2K_dpdp(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, float h, float w, float p):
+def d2K_dpdp(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w, double p):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef float d
+    cdef double d
     for i in xrange(x1_s):
         for j in xrange(x2_s):
             d = x1[i] - x2[j]
