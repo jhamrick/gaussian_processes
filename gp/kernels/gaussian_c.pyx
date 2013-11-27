0215d686821171ea3c1ff56d46b0f70e616b0042
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-11-26 19:47:07 -0800

    More efficient gaussian kernel computations

--- gp/kernels/gaussian_c.pyx
+++ gp/kernels/gaussian_c.pyx
@@ -3,113 +3,161 @@ from __future__ import division
 import numpy as np
 cimport numpy as np
 
-from math import sqrt, exp, pi
-
 DTYPE = np.float64
 ctypedef np.float64_t DTYPE_t
 
-def K(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
+cdef DTYPE_t SQRT_2_DIV_PI = np.sqrt(2.0 / np.pi)
+cdef DTYPE_t MIN = np.log(np.exp2(DTYPE(np.finfo(DTYPE).minexp + 4)))
+
+
+def K(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef double d
+    cdef DTYPE_t h2, w2, c1, c2, e
+
+    h2 = h ** 2
+    w2 = w ** 2
+
+    c1 = -0.5/w2
+    c2 = 0.5*SQRT_2_DIV_PI*h2/w
+
     for i in xrange(x1_s):
         for j in xrange(x2_s):
-            d = x1[i] - x2[j]
-            out[i, j] = 0.5*sqrt(2)*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*sqrt(w**2))
+            e = c1*(x1[i] - x2[j])**2
+            if e < MIN:
+                out[i, j] = 0
+            else:
+                try:
+                    out[i, j] = c2*np.exp(e)
+                except FloatingPointError:
+                    print c2, e, MIN
+                    raise
 
 
-def jacobian(np.ndarray[DTYPE_t, ndim=3] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
+def jacobian(np.ndarray[DTYPE_t, ndim=3] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
+    dK_dh(out[0], x1, x2, h, w)
+    dK_dw(out[1], x1, x2, h, w)
+
+
+def hessian(np.ndarray[DTYPE_t, ndim=4] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
+    d2K_dhdh(out[0, 0], x1, x2, h, w)
+    d2K_dhdw(out[0, 1], x1, x2, h, w)
+    d2K_dwdh(out[1, 0], x1, x2, h, w)
+    d2K_dwdw(out[1, 1], x1, x2, h, w)
+
+
+def dK_dh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef double d
+    cdef DTYPE_t h2, w2, c1, c2, e
+
+    h2 = h ** 2
+    w2 = w ** 2
+
+    c1 = -0.5/w2
+    c2 = SQRT_2_DIV_PI*h/w
+
     for i in xrange(x1_s):
         for j in xrange(x2_s):
-            d = x1[i] - x2[j]
-            out[0, i, j] = sqrt(2)*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*sqrt(w**2))
-            out[1, i, j] = 0.5*sqrt(2)*d**2*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**3*sqrt(w**2)) - 0.5*sqrt(2)*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w*sqrt(w**2))
+            e = c1*(x1[i] - x2[j])**2
+            if e < MIN:
+                out[i, j] = 0
+            else:
+                out[i, j] = c2*np.exp(e)
 
 
-def hessian(np.ndarray[DTYPE_t, ndim=4] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
+def dK_dw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef double d
+    cdef DTYPE_t h2, w2, c1, c2, c3, d2, e
+
+    h2 = h ** 2
+    w2 = w ** 2
+
+    c1 = -0.5/w2
+    c2 = 0.5*SQRT_2_DIV_PI*h2/w2
+    c3 = 0.5*SQRT_2_DIV_PI*h2/w**4
+
     for i in xrange(x1_s):
         for j in xrange(x2_s):
-            d = x1[i] - x2[j]
-            # h
-            out[0, 0, i, j] = sqrt(2)*exp(-0.5*d**2/w**2)/(sqrt(pi)*sqrt(w**2))
-            out[0, 1, i, j] = sqrt(2)*d**2*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**3*sqrt(w**2)) - sqrt(2)*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w*sqrt(w**2))
-            # w
-            out[1, 0, i, j] = sqrt(2)*d**2*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**3*sqrt(w**2)) - sqrt(2)*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w*sqrt(w**2))
-            out[1, 1, i, j] = 0.5*sqrt(2)*d**4*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**6*sqrt(w**2)) - 2.5*sqrt(2)*d**2*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**4*sqrt(w**2)) + sqrt(2)*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**2*sqrt(w**2))
+            d2 = (x1[i] - x2[j])**2
+            e = c1*d2
+            if e < MIN:
+                out[i, j] = 0
+            else:
+                out[i, j] = np.exp(e)*(c3*d2 - c2)
 
 
-def dK_dh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
+def d2K_dhdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef double d
+    cdef DTYPE_t h2, w2, c1, c2, e
+
+    h2 = h ** 2
+    w2 = w ** 2
+
+    c1 = -0.5/w2
+    c2 = SQRT_2_DIV_PI/w
+
     for i in xrange(x1_s):
         for j in xrange(x2_s):
-            d = x1[i] - x2[j]
-            out[i, j] = sqrt(2)*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*sqrt(w**2))
+            e = c1*(x1[i] - x2[j])**2
+            if e < MIN:
+                out[i, j] = 0
+            else:
+                out[i, j] = c2*np.exp(e)
 
 
-def dK_dw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
+def d2K_dhdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef double d
+    cdef DTYPE_t h2, w2, c1, c2, c3, d2, e
+
+    h2 = h ** 2
+    w2 = w ** 2
+
+    c1 = -0.5/w2
+    c2 = SQRT_2_DIV_PI*h/w2
+    c3 = SQRT_2_DIV_PI*h/w**4
+
     for i in xrange(x1_s):
         for j in xrange(x2_s):
-            d = x1[i] - x2[j]
-            out[i, j] = 0.5*sqrt(2)*d**2*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**3*sqrt(w**2)) - 0.5*sqrt(2)*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w*sqrt(w**2))
+            d2 = (x1[i] - x2[j])**2
+            e = c1*d2
+            if e < MIN:
+                out[i, j] = 0
+            else:
+                out[i, j] = np.exp(e)*(c3*d2 - c2)
 
 
-def d2K_dhdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
+def d2K_dwdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
+    d2K_dhdw(out, x1, x2, h, w)
+
+
+def d2K_dwdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
     cdef int x1_s = x1.size
     cdef int x2_s = x2.size
     cdef int i, j
-    cdef double d
+    cdef DTYPE_t h2, w2, c1, c2, c3, c4, d2, e
+
+    h2 = h ** 2
+    w2 = w ** 2
+
+    c1 = -0.5/w2
+    c2 = SQRT_2_DIV_PI*h2/w**3
+    c3 = 2.5*SQRT_2_DIV_PI*h2/w**5
+    c4 = 0.5*SQRT_2_DIV_PI*h2/w**7
+
     for i in xrange(x1_s):
         for j in xrange(x2_s):
-            d = x1[i] - x2[j]
-            out[i, j] = sqrt(2)*exp(-0.5*d**2/w**2)/(sqrt(pi)*sqrt(w**2))
-
-
-def d2K_dhdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
-    cdef int x1_s = x1.size
-    cdef int x2_s = x2.size
-    cdef int i, j
-    cdef double d
-    for i in xrange(x1_s):
-        for j in xrange(x2_s):
-            d = x1[i] - x2[j]
-            out[i, j] = sqrt(2)*d**2*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**3*sqrt(w**2)) - sqrt(2)*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w*sqrt(w**2))
-
-
-def d2K_dwdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
-    cdef int x1_s = x1.size
-    cdef int x2_s = x2.size
-    cdef int i, j
-    cdef double d
-    for i in xrange(x1_s):
-        for j in xrange(x2_s):
-            d = x1[i] - x2[j]
-            out[i, j] = sqrt(2)*d**2*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**3*sqrt(w**2)) - sqrt(2)*h*exp(-0.5*d**2/w**2)/(sqrt(pi)*w*sqrt(w**2))
-
-
-def d2K_dwdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, double h, double w):
-    cdef int x1_s = x1.size
-    cdef int x2_s = x2.size
-    cdef int i, j
-    cdef double d
-    for i in xrange(x1_s):
-        for j in xrange(x2_s):
-            d = x1[i] - x2[j]
-            out[i, j] = 0.5*sqrt(2)*d**4*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**6*sqrt(w**2)) - 2.5*sqrt(2)*d**2*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**4*sqrt(w**2)) + sqrt(2)*h**2*exp(-0.5*d**2/w**2)/(sqrt(pi)*w**2*sqrt(w**2))
-
-
+            d2 = (x1[i] - x2[j])**2
+            e = c1*d2
+            if e < MIN:
+                out[i, j] = 0
+            else:
+                out[i, j] = np.exp(e)*(c4*d2**2 - c3*d2 + c2)
