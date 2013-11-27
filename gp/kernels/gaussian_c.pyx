cad1c26a47fbbb4e1250e9f22b7aa3e95f6750d3
Jessica B. Hamrick <jhamrick@berkeley.edu>
2013-11-26 20:29:05 -0800

    Small performance tweaks to gaussian_c.pyx

--- gp/kernels/gaussian_c.pyx
+++ gp/kernels/gaussian_c.pyx
@@ -1,13 +1,19 @@
+#cython: boundscheck=False
+
 from __future__ import division
 
 import numpy as np
 cimport numpy as np
 
+cdef log = np.log
+cdef exp = np.exp
+cdef sqrt = np.sqrt
+
 DTYPE = np.float64
 ctypedef np.float64_t DTYPE_t
 
-cdef DTYPE_t SQRT_2_DIV_PI = np.sqrt(2.0 / np.pi)
-cdef DTYPE_t MIN = np.log(np.exp2(DTYPE(np.finfo(DTYPE).minexp + 4)))
+cdef DTYPE_t SQRT_2_DIV_PI = sqrt(2.0 / np.pi)
+cdef DTYPE_t MIN = log(np.exp2(DTYPE(np.finfo(DTYPE).minexp + 4)))
 
 
 def K(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
@@ -29,7 +35,7 @@ def K(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarra
                 out[i, j] = 0
             else:
                 try:
-                    out[i, j] = c2*np.exp(e)
+                    out[i, j] = c2*exp(e)
                 except FloatingPointError:
                     print c2, e, MIN
                     raise
@@ -65,7 +71,7 @@ def dK_dh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.nd
             if e < MIN:
                 out[i, j] = 0
             else:
-                out[i, j] = c2*np.exp(e)
+                out[i, j] = c2*exp(e)
 
 
 def dK_dw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
@@ -88,7 +94,7 @@ def dK_dw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.nd
             if e < MIN:
                 out[i, j] = 0
             else:
-                out[i, j] = np.exp(e)*(c3*d2 - c2)
+                out[i, j] = exp(e)*(c3*d2 - c2)
 
 
 def d2K_dhdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
@@ -109,7 +115,7 @@ def d2K_dhdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np
             if e < MIN:
                 out[i, j] = 0
             else:
-                out[i, j] = c2*np.exp(e)
+                out[i, j] = c2*exp(e)
 
 
 def d2K_dhdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
@@ -132,7 +138,7 @@ def d2K_dhdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np
             if e < MIN:
                 out[i, j] = 0
             else:
-                out[i, j] = np.exp(e)*(c3*d2 - c2)
+                out[i, j] = exp(e)*(c3*d2 - c2)
 
 
 def d2K_dwdh(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np.ndarray[DTYPE_t, ndim=1] x2, DTYPE_t h, DTYPE_t w):
@@ -160,4 +166,4 @@ def d2K_dwdw(np.ndarray[DTYPE_t, ndim=2] out, np.ndarray[DTYPE_t, ndim=1] x1, np
             if e < MIN:
                 out[i, j] = 0
             else:
-                out[i, j] = np.exp(e)*(c4*d2**2 - c3*d2 + c2)
+                out[i, j] = exp(e)*(c4*d2**2 - c3*d2 + c2)
